e1a54769ea851b5498a44d777dd80634c9c77c2a
ATawzer <34928044+ATawzer@users.noreply.github.com>
2023-04-20 11:31:36 -0600

    zip and update tasks working as expected

--- tasks.py
+++ tasks.py
@@ -15,25 +15,26 @@ def update(c, blender_version="3.5"):
     dir1 = './qar'
     dir2 = f"{user_dir}/AppData/Roaming/Blender Foundation/Blender/{blender_version}/scripts/addons/qar"
 
-    # Get a list of all the files in directory1
-    files = os.listdir(dir1)
+    if os.path.exists(dir2):
+        shutil.rmtree(dir2)
 
     # Loop through each file and copy it to directory2
-    for file in files:
-        # Construct full file paths
-        src_file = os.path.join(dir1, file)
-        dest_file = os.path.join(dir2, file)
-        # Copy file to directory2
-        shutil.copy(src_file, dest_file)
+    shutil.copytree(dir1, dir2)
 
-    # Create a zip file with the same name as the last part of dir2
+@task
+def zip(c):
+    """
+    Creates a zip file of the addon
+    """
+
+    dir1 = './qar'
+
+    # Create a zip file with the same name as the last part of dir1
     zip_name = os.path.basename(dir1)
-    zip_file = zipfile.ZipFile(zip_name + '.zip', 'w')
-
-    # Add all files in dir2 to the zip file
-    for file in os.listdir(dir1):
-        file_path = os.path.join(dir1, file)
-        zip_file.write(file_path, file)
-
-    # Close the zip file
-    zip_file.close()
+    with zipfile.ZipFile(zip_name + '.zip', 'w') as zip_file:
+        for foldername, subfolders, filenames in os.walk(dir1):
+            for filename in filenames:
+                # Create the full filepath by using os.path.join()
+                filepath = os.path.join(foldername, filename)
+                # Add the file to the zip file
+                zip_file.write(filepath, os.path.relpath(filepath, dir1))
